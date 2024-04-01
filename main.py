e69a7f939f0e419c2d8c4fa8701039aeecc5d907
BARKEM-JC <57711861+BARKEM-JC@users.noreply.github.com>
2024-04-02 07:21:50 +1000

    bloop

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ if __name__ == '__main__':
     #kill_all_selenium_instances()
     #atexit.register(kill_all_selenium_instances)
     start_time = time.time()
-    search = Search('eggs', SearchConfig(
-        ignore_text=False, humans_only=False, image_limit=50, chrome_options=ChromeConfig(debug_mode), scraper_processes=1,
-        related=False))
+    search = Search('eggs', SearchConfig(image_limit=50, chrome_options=ChromeConfig(debug_mode)))
     search.StartThreads()
     print("Execution Time: ", time.time() - start_time - 5)
